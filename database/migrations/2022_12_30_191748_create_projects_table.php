e7fea619bc2198d64557365ac0d91a99eb3f863f
Radosław Malinowski <malinowski.rad@gmail.com>
2022-12-30 20:20:44 +0100

    create projects table migration

---
+++ database/migrations/2022_12_30_191748_create_projects_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('subject');
+            $table->text('description');
+            $table->date('startDate');
+            $table->date('endDate');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projects');
+    }
+};
