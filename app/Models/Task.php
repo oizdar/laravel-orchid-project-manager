7fb08a483f342dae205a846df6e8aca17afc3cae
Radosław Malinowski <malinowski.rad@gmail.com>
2023-01-02 11:02:25 +0100

    create Task Model with factory

---
+++ app/Models/Task.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Orchid\Filters\Filterable;
+use Orchid\Screen\AsSource;
+
+class Task extends Model
+{
+    use HasFactory, AsSource, Filterable;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'start_date',
+        'end_date',
+        'status',
+        'user_id',
+        'project_id',
+    ];
+
+    protected $allowedSorts = [
+        'name',
+        'start_date',
+        'end_date',
+        'status',
+        'user_id',
+        'project_id',
+    ];
+
+    protected $allowedFilters = [
+        'name',
+        'start_date',
+        'end_date',
+        'status',
+        'user_id',
+        'project_id',
+    ];
+}
---
+++ database/factories/TaskFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Enums\TaskStatusesEnum;
+use App\Models\Project;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Project>
+ */
+class TaskFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => fake()->text(50),
+            'description' => fake()->text(250),
+            'start_date' => fake()->dateTimeBetween('tomorrow', '+1 month')->format('Y-m-d'),
+            'end_date' => null,
+            'status' => fake()->randomElement(array_column(TaskStatusesEnum::cases(), 'value')),
+            'user_id' => User::all('id')->random()->id ?? null,
+            'project_id' => Project::all('id')->random()->id,
+        ];
+    }
+}
